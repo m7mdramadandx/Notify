5496a94295de85c7ec98ce1b94ece9f991b4fa83
mohamed.abdelmawgoud <Mohamed.Abdelmawgoud@evapharma.com>
2022-05-27 13:59:51 +0200

    Initial commit

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    const val minSDK = 23
+    const val targetSDK = 31
+    const val versionCode = 5
+    const val versionName = "2.0.0"
+    const val compileSDK = 31
+    const val applicationId = "com.ramadan.notify"
+    const val buildToolsVersion = "31.0.0"
+    const val applicationIdStaging = "com.ramadan.notify.staging"
+}
