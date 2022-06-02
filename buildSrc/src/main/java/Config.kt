2462d13dd5be63cf5048285ec7ef987207bfef45
Muhammad Ramadan <mhmdrmdandx@gmail.com>
2022-06-02 14:08:30 +0200

    Update Config.kt

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,6 +1,6 @@
 object Config {
-    const val versionCode = 5
-    const val versionName = "2.0.0"
+    const val versionCode = 6
+    const val versionName = "2.0.1"
     const val minSDK = 23
     const val targetSDK = 31
     const val compileSDK = 31
